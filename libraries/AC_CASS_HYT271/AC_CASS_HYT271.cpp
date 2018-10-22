2686145b1ba8ceea2041130e38e6e4a09a046374
tony2157 <tony.segales700@gmail.com>
2018-10-22 11:47:52 -0500

    Add CASS HYT271 libs in libraries folder

---
+++ libraries/AC_CASS_HYT271/AC_CASS_HYT271.cpp
@@ -0,0 +1,108 @@
+#include "AC_CASS_HYT271.h"
+
+#include <utility>
+#include <stdio.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Math/AP_Math.h>
+
+extern const AP_HAL::HAL &hal;
+
+AC_CASS_HYT271::AC_CASS_HYT271() :
+    _dev(nullptr),
+    _temperature(0),
+    _humidity(0),
+    _healthy(false)
+{
+}
+
+bool AC_CASS_HYT271::init()
+{
+
+    sem = hal.util->new_semaphore();
+
+    // Bus 0 is for Pixhawk 2.1 I2C and Bus 1 is for Pixhawk 1 and PixRacer I2C
+    _dev = std::move(hal.i2c_mgr->get_device(0, HYT271_DEFAULT_ADDR));
+    if (!_dev) {
+        printf("HYT271 device is null!");
+        return false;
+    }
+
+    if (!_dev->get_semaphore()->take(0)) {
+        AP_HAL::panic("PANIC: HYT271: failed to take serial semaphore for init");
+    }
+
+    _dev->set_retries(10);
+
+    if (!_measure()) {
+        printf("HYT271 read failed");
+        _dev->get_semaphore()->give();
+        return false;
+    }
+
+    // lower retries for run
+    _dev->set_retries(3);
+
+    _dev->get_semaphore()->give();
+
+    /* Request 20Hz update */
+    // Max conversion time is 12 ms
+    _dev->register_periodic_callback(60000,
+                                     FUNCTOR_BIND_MEMBER(&AC_CASS_HYT271::_timer, void));
+    return true;
+}
+
+void AC_CASS_HYT271::set_i2c_addr(uint8_t addr){
+    if (_dev) {
+        _dev->set_address(addr);
+    }
+}
+
+bool AC_CASS_HYT271::_measure()
+{
+    uint8_t cmd = 0x00;
+        if (!_dev->transfer(&cmd, 1, nullptr, 0)) {
+            return false;
+        }
+        return true;
+}
+
+bool AC_CASS_HYT271::_collect(float &hum, float &temp)
+{
+    uint8_t data[4];
+    int16_t raw;
+    //Read sensors
+    if (!_dev->transfer(nullptr, 0, data, sizeof(data))) {
+        return false;
+    }
+
+    if ((data[0] & 0x40) == 0x40){
+        return false;
+    }
+
+    raw = (data[0] << 8) | data[1];
+    raw = raw & 0x3FFF;
+
+    hum = (100.0 / ((float)pow(2,14) - 1)) * (float)raw;
+
+    data[3] = (data[3] >> 2);
+    raw = (data[2] << 6) | data[3];
+    temp = (165.0 / ((float)pow(2,14) - 1)) * (float)raw + 233.15f;
+
+    return true;  
+}
+
+void AC_CASS_HYT271::_timer(void)
+{
+    if(sem->take(HAL_SEMAPHORE_BLOCK_FOREVER)){
+        if (_healthy) {
+            _collect(_humidity, _temperature);
+        } else {
+            _temperature = 0;
+            _humidity = 0;
+        }
+
+        _healthy = _measure();
+        
+        sem->give();
+    }
+}
