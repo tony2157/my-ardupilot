ee1d1388a516760a5056125945059e656d0712e6
tony2157 <tony.segales700@gmail.com>
2018-10-22 11:29:00 -0500

    Set to 10Hz all user functions

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -150,10 +150,10 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK(userhook_MediumLoop,   10,     75),
 #endif
 #ifdef USERHOOK_SLOWLOOP
-    SCHED_TASK(userhook_SlowLoop,     3.3,    75),
+    SCHED_TASK(userhook_SlowLoop,     10,    75),
 #endif
 #ifdef USERHOOK_SUPERSLOWLOOP
-    SCHED_TASK(userhook_SuperSlowLoop, 1,   75),
+    SCHED_TASK(userhook_SuperSlowLoop, 10,   75),
 #endif
     SCHED_TASK(button_update,          5,    100),
     SCHED_TASK(stats_update,           1,    100),
