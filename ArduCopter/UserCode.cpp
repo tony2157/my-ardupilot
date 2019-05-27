243b559a17e11407ec40f8a2c5846765f415f56c
tony2157 <tony.segales700@gmail.com>
2019-05-27 11:24:54 -0500

    Improve handling of the stored variables, less steps more efficient

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -4,13 +4,9 @@
 
 //Humidity sensor Params
 const int N_RH = 4;     //supports up to 4
-float raw_H[4], rawRHt[4];
-uint8_t healthy_RH[4];
 
 //IMET sensor Params
 const int N_imet = 4;   //supports up to 4
-float resist[4], curr[4];
-uint8_t healthy_imet[4];
 
 //Fan control params
 //It will run the fan at SERVO_MAX, which can be set in the params list
@@ -35,14 +31,6 @@ void Copter::userhook_init()
 {
     // put your initialisation code here
     // this will be called once at start-up
-
-    //Initialize Global Variables
-    memset(raw_H,0,sizeof(raw_H));
-    memset(rawRHt,0,sizeof(rawRHt));
-    memset(resist,0,sizeof(resist));
-    memset(curr,0,sizeof(curr));
-    memset(healthy_imet,0,sizeof(healthy_imet));
-    memset(healthy_RH,0,sizeof(healthy_RH));
 
     //Initialize Wind estimator
     _wind_dir = 0.0f; _roll = 0.0f; _pitch = 0.0f; _yaw = 0.0f;
@@ -79,23 +67,26 @@ void Copter::userhook_50Hz()
 #ifdef USERHOOK_MEDIUMLOOP
 void Copter::userhook_MediumLoop()
 {
-    // Read temperature
-    curr[0] = copter.CASS_Imet[0].temperature();
-    curr[1] = copter.CASS_Imet[1].temperature();
-    curr[2] = copter.CASS_Imet[2].temperature();
-    curr[3] = copter.CASS_Imet[3].temperature();
-    // Read resistance
-    resist[0] = copter.CASS_Imet[0].resistance();
-    resist[1] = copter.CASS_Imet[1].resistance();
-    resist[2] = copter.CASS_Imet[2].resistance();
-    resist[3] = copter.CASS_Imet[3].resistance();
-    // Read Health
-    healthy_imet[0] = copter.CASS_Imet[0].healthy();
-    healthy_imet[1] = copter.CASS_Imet[1].healthy();
-    healthy_imet[2] = copter.CASS_Imet[2].healthy();
-    healthy_imet[3] = copter.CASS_Imet[3].healthy();
-
-    #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+    #if CONFIG_HAL_BOARD != HAL_BOARD_SITL
+        // Read Temperature, Resistance and Health. Write sensors packet into the SD card
+        struct log_IMET pkt_temp = {
+            LOG_PACKET_HEADER_INIT(LOG_IMET_MSG),
+            time_stamp             : AP_HAL::micros64(),                //Store time in microseconds
+            fan_status             : _fan_status,                       //Store Fan on/off status
+            healthy1               : copter.CASS_Imet[0].healthy(),     //Store sensor health
+            healthy2               : copter.CASS_Imet[1].healthy(),
+            healthy3               : copter.CASS_Imet[2].healthy(),
+            healthy4               : copter.CASS_Imet[3].healthy(),
+            temperature1           : copter.CASS_Imet[0].temperature(), //Store iMet Temperature
+            temperature2           : copter.CASS_Imet[1].temperature(),
+            temperature3           : copter.CASS_Imet[2].temperature(),
+            temperature4           : copter.CASS_Imet[3].temperature(),
+            resist1                : copter.CASS_Imet[0].resistance(),  //Store iMet bead resistence
+            resist2                : copter.CASS_Imet[1].resistance(),
+            resist3                : copter.CASS_Imet[2].resistance(),
+            resist4                : copter.CASS_Imet[3].resistance()
+        }; 
+    #else
         uint32_t m = AP_HAL::millis();
         float alt; float simT;
         copter.ahrs.get_relative_position_D_home(alt);
@@ -112,93 +103,71 @@ void Copter::userhook_MediumLoop()
         else{
             simT = 298;
         }
-        curr[0] = simT + sin(0.0005*m) * 0.001;
-        curr[1] = 2 + curr[0] + sin(0.0006*m) * 0.001;
-        curr[2] = 2 + curr[1] + sin(0.0004*m) * 0.001;
-        curr[3] = 2 + curr[2] + sin(0.0007*m) * 0.001; 
+        // Write simulated sensors packet into the SD card
+        struct log_IMET pkt_temp = {
+            LOG_PACKET_HEADER_INIT(LOG_IMET_MSG),
+            time_stamp             : AP_HAL::micros64(),
+            fan_status             : _fan_status,
+            healthy1               : copter.CASS_Imet[0].healthy(),
+            healthy2               : copter.CASS_Imet[1].healthy(),
+            healthy3               : copter.CASS_Imet[2].healthy(),
+            healthy4               : copter.CASS_Imet[3].healthy(),
+            temperature1           : simT + sinf(0.0003f*m) * 0.001f,
+            temperature2           : simT + sinf(0.0004f*m) * 0.001f,
+            temperature3           : simT + sinf(0.0005f*m) * 0.001f,
+            temperature4           : simT + sinf(0.0006f*m) * 0.001f,
+            resist1                : copter.CASS_Imet[0].resistance(),
+            resist2                : copter.CASS_Imet[1].resistance(),
+            resist3                : copter.CASS_Imet[2].resistance(),
+            resist4                : copter.CASS_Imet[3].resistance()
+        };
     #endif
-
-    // Write sensors packet into the SD card
-    struct log_IMET pkt_temp = {
-        LOG_PACKET_HEADER_INIT(LOG_IMET_MSG),
-        time_stamp             : AP_HAL::micros64(),
-        fan_status             : _fan_status,
-        healthy1               : healthy_imet[0],
-        healthy2               : healthy_imet[1],
-        healthy3               : healthy_imet[2],
-        healthy4               : healthy_imet[3],
-        temperature1           : curr[0],
-        temperature2           : curr[1],
-        temperature3           : curr[2],
-        temperature4           : curr[3],
-        resist1                : resist[0],
-        resist2                : resist[1],
-        resist3                : resist[2],
-        resist4                : resist[3]
-    };
-    copter.DataFlash.WriteBlock(&pkt_temp, sizeof(pkt_temp));
-
-    // Send data to ground station
-    //copter.send_cass_data(0, curr, 4);
-
-    // cliSerial->printf("\ncurr3: %f", curr[3]);
+    copter.DataFlash.WriteBlock(&pkt_temp, sizeof(pkt_temp));   //Send package to SD card
 }
 #endif
 
 #ifdef USERHOOK_SLOWLOOP
 void Copter::userhook_SlowLoop()
 {
-    // Read Realative Humidity
-    raw_H[0] = copter.CASS_HYT271[0].relative_humidity();
-    raw_H[1] = copter.CASS_HYT271[1].relative_humidity();
-    raw_H[2] = copter.CASS_HYT271[2].relative_humidity();
-    raw_H[3] = copter.CASS_HYT271[3].relative_humidity();
-    // Read Temperature
-    rawRHt[0] = copter.CASS_HYT271[0].temperature();
-    rawRHt[1] = copter.CASS_HYT271[1].temperature();
-    rawRHt[2] = copter.CASS_HYT271[2].temperature(); 
-    rawRHt[3] = copter.CASS_HYT271[3].temperature();
-    // Read Health
-    healthy_RH[0] = copter.CASS_HYT271[0].healthy();
-    healthy_RH[1] = copter.CASS_HYT271[1].healthy();
-    healthy_RH[2] = copter.CASS_HYT271[2].healthy();
-    healthy_RH[3] = copter.CASS_HYT271[3].healthy();
-
-    #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+    #if CONFIG_HAL_BOARD != HAL_BOARD_SITL
+        // Read Rel. Humidity, Temperature and Health. Write sensors packet into the SD card
+        struct log_RH pkt_RH = {
+            LOG_PACKET_HEADER_INIT(LOG_RH_MSG),
+            time_stamp             : AP_HAL::micros64(),                        //Store time in microseconds
+            healthy1               : copter.CASS_HYT271[0].healthy(),           //Store senors health
+            healthy2               : copter.CASS_HYT271[1].healthy(),
+            healthy3               : copter.CASS_HYT271[2].healthy(),
+            healthy4               : copter.CASS_HYT271[3].healthy(),
+            humidity1              : copter.CASS_HYT271[0].relative_humidity(), //Store Rel. humidity
+            humidity2              : copter.CASS_HYT271[1].relative_humidity(),
+            humidity3              : copter.CASS_HYT271[2].relative_humidity(),
+            humidity4              : copter.CASS_HYT271[3].relative_humidity(),
+            RHtemp1                : copter.CASS_HYT271[0].temperature(),       //Store temperature
+            RHtemp2                : copter.CASS_HYT271[1].temperature(),
+            RHtemp3                : copter.CASS_HYT271[2].temperature(),
+            RHtemp4                : copter.CASS_HYT271[3].temperature()
+        };
+    #else
         uint32_t m = AP_HAL::millis();
-        rawRHt[0] = 298.15 + sin(0.0003*m) * 2;
-        rawRHt[1] = 1 + rawRHt[0];
-        rawRHt[2] = 1 + rawRHt[1];
-        rawRHt[3] = 1 + rawRHt[2];
-
-        raw_H[0] = abs(sin(m) * 95);
-        raw_H[1] = 1 + raw_H[0];
-        raw_H[2] = 1 + raw_H[1];
-        raw_H[3] = 1 + raw_H[2];
-    #endif   
-    
-    // Write sensors packet into the SD card
-    struct log_RH pkt_RH = {
-        LOG_PACKET_HEADER_INIT(LOG_RH_MSG),
-        time_stamp             : AP_HAL::micros64(), //- _last_read_ms),
-        healthy1               : healthy_RH[0],
-        healthy2               : healthy_RH[1],
-        healthy3               : healthy_RH[2],
-        healthy4               : healthy_RH[3],
-        humidity1              : raw_H[0],
-        humidity2              : raw_H[1],
-        humidity3              : raw_H[2],
-        humidity4              : raw_H[3],
-        RHtemp1                : rawRHt[0],
-        RHtemp2                : rawRHt[1],
-        RHtemp3                : rawRHt[2],
-        RHtemp4                : rawRHt[3]
-    };
-    copter.DataFlash.WriteBlock(&pkt_RH, sizeof(pkt_RH));
-
-    //Send data to ground station
-    //copter.send_cass_data(1, raw_H, 4);
-    //copter.send_cass_data(2, rawRHt, 4);  
+        // Write sensors packet into the SD card
+        struct log_RH pkt_RH = {
+            LOG_PACKET_HEADER_INIT(LOG_RH_MSG),
+            time_stamp             : AP_HAL::micros64(),
+            healthy1               : copter.CASS_HYT271[0].healthy(),
+            healthy2               : copter.CASS_HYT271[1].healthy(),
+            healthy3               : copter.CASS_HYT271[2].healthy(),
+            healthy4               : copter.CASS_HYT271[3].healthy(),
+            humidity1              : fabs(sinf(0.0003f*m) * 60.0f),
+            humidity2              : fabs(sinf(0.0004f*m) * 60.0f),
+            humidity3              : fabs(sinf(0.0005f*m) * 60.0f),
+            humidity4              : fabs(sinf(0.0006f*m) * 60.0f),
+            RHtemp1                : 298.15f + sinf(0.0003f*m) * 2.0f,
+            RHtemp2                : 298.15f + sinf(0.0004f*m) * 2.0f,
+            RHtemp3                : 298.15f + sinf(0.0005f*m) * 2.0f,
+            RHtemp4                : 298.15f + sinf(0.0006f*m) * 2.0f
+        };
+    #endif
+    copter.DataFlash.WriteBlock(&pkt_RH, sizeof(pkt_RH));   //Send package to SD card
 }
 #endif
 
