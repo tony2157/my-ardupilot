6dfb74278e66a561424a1e0d94d03a76dded3d00
tony2157 <tony.segales700@gmail.com>
2020-03-20 18:17:36 -0500

    Increase wind vane function call rate to 20Hz

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -184,7 +184,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK(userhook_SlowLoop,     10,    75),
 #endif
 #ifdef USERHOOK_SUPERSLOWLOOP
-    SCHED_TASK(userhook_SuperSlowLoop, 10,   75),
+    SCHED_TASK(userhook_SuperSlowLoop, 20,   75),
 #endif
     SCHED_TASK_CLASS(AP_Button,            &copter.g2.button,           update,           5, 100),
 #if STATS_ENABLED == ENABLED
