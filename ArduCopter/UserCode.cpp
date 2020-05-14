a0e8e382529744341282f19268962dc8ab68da5c
tony2157 <tony.segales700@gmail.com>
2020-05-14 13:13:29 -0500

    Trigger wvane auto RTL only in auto modes

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -290,11 +290,13 @@ void Copter::userhook_SuperSlowLoop()
         }
 
         //Switch to RTL automatically if wind speed is too high (in m/s)
-        //If tolerance is set to zero then function is disabled
-        if(!is_zero(g.wind_vane_spd_tol)){
-            if(_wind_speed > g.wind_vane_spd_tol && high_wind_flag == false){
-                gcs().send_text(MAV_SEVERITY_WARNING, "Switched to RTL due to very high wind");
-                copter.set_mode(RTL, MODE_REASON_UNKNOWN);
+        //If tolerance is set to zero then auto RTL is disabled but it will still warn if enabled
+        if(!is_zero(g.wind_vane_enabled)){
+            if(_wind_speed > g.wind_vane_spd_tol && high_wind_flag == false && copter.flightmode->is_autopilot()){
+                gcs().send_text(MAV_SEVERITY_WARNING, "Warning high wind: Switched to RTL");
+                if(!is_zero(g.wind_vane_spd_tol)){
+                    copter.set_mode(RTL, MODE_REASON_UNKNOWN);
+                }
                 high_wind_flag = true;
             }
             else if(_wind_speed < (g.wind_vane_spd_tol - 3.0f) && high_wind_flag == true){
