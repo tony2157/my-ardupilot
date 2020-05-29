12c0ee1c09c06350b7c8dd3bda6a23c9eaa4723b
tony2157 <tony.segales700@gmail.com>
2020-05-29 18:30:30 -0500

    Remove 1st order filter for the wind profile in the simulator

--- libraries/AP_HAL_SITL/SITL_State.cpp
+++ libraries/AP_HAL_SITL/SITL_State.cpp
@@ -378,7 +378,7 @@ void SITL_State::_simulator_servos(SITL::Aircraft::sitl_input &input)
             if(altitude<1030){
                 wind_direction = -(2.82e-11f)*powf(altitude,5) + (6.71451e-8f)*powf(altitude,4) - (5.62104222e-5f)*powf(altitude,3) + (2.00937168714e-2f)*powf(altitude,2) - 2.9089387983472f*altitude + 235.34632f;
                 wind_direction = fmodf(wind_direction,360.0f);
-                wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*wind_direction);
+                //wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*wind_direction);
             }
             else{
                 wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*_sitl->wind_direction);
@@ -388,7 +388,7 @@ void SITL_State::_simulator_servos(SITL::Aircraft::sitl_input &input)
             if(altitude<1030){
                 wind_direction = 45*sinf(1.88e-4f*altitude*altitude) + 60;
                 wind_direction = fmodf(wind_direction,360.0f);
-                wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*wind_direction);
+                //wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*wind_direction);
             }
             else{
                 wind_direction = _sitl->wind_direction_active = (0.95f*_sitl->wind_direction_active) + (0.05f*_sitl->wind_direction);
