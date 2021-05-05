0baa368ae74fb53eae86f67f9bf7a7db5e36e5ea
tony2157 <tony.segales700@gmail.com>
2021-05-05 16:13:48 -0500

    Create CASS mavlink message handlers

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -77,5 +77,7 @@ enum ap_message : uint8_t {
     MSG_EFI_STATUS,
     MSG_GENERATOR_STATUS,
     MSG_WINCH_STATUS,
+    MSG_CASS_IMET, //CASS message ID
+    MSG_CASS_HYT271,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
