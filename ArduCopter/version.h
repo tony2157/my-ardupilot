6e7d48d8775c191b39e1f649987c510a83f4d052
tony2157 <tony.segales700@gmail.com>
2022-03-16 17:13:25 -0500

    Label ARRC-Copter version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.1.5"
+#define THISFIRMWARE "ARRC-Copter V4.1.5"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,5,FIRMWARE_VERSION_TYPE_OFFICIAL
