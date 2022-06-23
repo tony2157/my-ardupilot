756490b8a2908b1e435fc6a7fce84ea1fd512781
tony2157 <tony.segales700@gmail.com>
2022-06-23 15:35:29 -0500

    ARRC-Gimbal: Change roll and pitch signs

--- libraries/AP_Mount/AP_Mount_Alexmos.cpp
+++ libraries/AP_Mount/AP_Mount_Alexmos.cpp
@@ -159,7 +159,7 @@ void AP_Mount_Alexmos::control_axis(const Vector3f& angle, bool targets_in_degre
     outgoing_buffer.angle_speed.speed_roll = DEGREE_PER_SEC_TO_VALUE(AP_MOUNT_ALEXMOS_SPEED);
     outgoing_buffer.angle_speed.angle_roll = DEGREE_TO_VALUE(target_deg.x);
     outgoing_buffer.angle_speed.speed_pitch = DEGREE_PER_SEC_TO_VALUE(AP_MOUNT_ALEXMOS_SPEED);
-    outgoing_buffer.angle_speed.angle_pitch = DEGREE_TO_VALUE(-1*target_deg.y);
+    outgoing_buffer.angle_speed.angle_pitch = DEGREE_TO_VALUE(target_deg.y);
     outgoing_buffer.angle_speed.speed_yaw = DEGREE_PER_SEC_TO_VALUE(AP_MOUNT_ALEXMOS_SPEED);
     outgoing_buffer.angle_speed.angle_yaw = DEGREE_TO_VALUE(target_deg.z);
     send_command(CMD_CONTROL, (uint8_t *)&outgoing_buffer.angle_speed, sizeof(alexmos_angles_speed));
@@ -177,9 +177,9 @@ void AP_Mount_Alexmos::control_axis_d(const Vector3d& angle , bool targets_in_de
     outgoing_buffer.angle_speed.mode_pitch = AP_MOUNT_ALEXMOS_MODE_ANGLE;
     outgoing_buffer.angle_speed.mode_yaw = AP_MOUNT_ALEXMOS_MODE_ANGLE_REL_FRAME;
     outgoing_buffer.angle_speed.speed_roll = DEGREE_PER_SEC_TO_VALUE_D(AP_MOUNT_ALEXMOS_SPEED);
-    outgoing_buffer.angle_speed.angle_roll = DEGREE_TO_VALUE_D(-1*target_deg.x);
+    outgoing_buffer.angle_speed.angle_roll = DEGREE_TO_VALUE_D(target_deg.x);
     outgoing_buffer.angle_speed.speed_pitch = DEGREE_PER_SEC_TO_VALUE_D(AP_MOUNT_ALEXMOS_SPEED);
-    outgoing_buffer.angle_speed.angle_pitch = DEGREE_TO_VALUE_D(-1*target_deg.y);
+    outgoing_buffer.angle_speed.angle_pitch = DEGREE_TO_VALUE_D(target_deg.y);
     outgoing_buffer.angle_speed.speed_yaw = DEGREE_PER_SEC_TO_VALUE_D(AP_MOUNT_ALEXMOS_SPEED);
     outgoing_buffer.angle_speed.angle_yaw = DEGREE_TO_VALUE_D(target_deg.z);
     send_command(CMD_CONTROL, (uint8_t *)&outgoing_buffer.angle_speed, sizeof(alexmos_angles_speed));
