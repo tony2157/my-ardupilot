dc02103e6a48267ec6c42c49592402db874e5138
tony2157 <tony.segales700@gmail.com>
2023-06-28 15:40:06 -0500

    ARRC_Gimbal: Include Polarization Loss factor formula to compute angle offset

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -9,11 +9,16 @@ const uint8_t gimbal_angle_span = 30;        // Must be an even number
 const uint8_t gimbal_step = 5;              // Angle steps
 const uint16_t gimbal_init_wait = 4000;      // Gimbal initial waiting time
 const uint16_t gimbal_wait = 3000;           // Waiting time while gimbal is rotating
-const uint16_t gimbal_sample_time = 2000;    // Sampling time at each angle step in milliseconds
+const uint16_t gimbal_sample_time = 2500;    // Sampling time at each angle step in milliseconds
 float gimbal_probe_samples[gimbal_angle_span/gimbal_step + 1];
 uint8_t gimbal_num_samples;
 bool alignment_done;
 Matrix3d rotm_step;
+
+float mean(const float* v, int n);
+float dot_product(const float* v1, const float* v2, int n);
+float standard_deviation(const float* v, float mean, int n);
+float correlation(const float* v1, const float* v2, int n);
 
 uint8_t kek;
 
@@ -80,133 +85,69 @@ void Copter::user_ARRC_gimbal()
             goto repeat;
         }
 
-        // // Simulated data points for code test and debugging
+        // Simulated data points for code test and debugging
         // if (kek == 0){
-        //     float newValues[] = {-10.6525, -10.2897, -10.0700, -10.0001, -10.0821, -10.3136, -10.6874}; // 0.2 deg offset, measured: -0.197742
+        //     float newValues[] = {-10.6525, -10.2897, -10.0700, -10.0001, -10.0821, -10.3136, -10.6874}; // -0.2 deg offset, measured: -0.200017
         //     for(int i = 0; i < 7; i++) {
         //         gimbal_probe_samples[i] = newValues[i];
         //     }
         //     kek = kek + 1;
         // }
         // else if (kek == 1){
-        //     float newValues[] = {-10.5450, -10.2185, -10.0373, -10.0069, -10.1281, -10.3975, -10.8066}; // 1.5 deg offset, measured: -1.484070
+        //     float newValues[] = {-10.5450, -10.2185, -10.0373, -10.0069, -10.1281, -10.3975, -10.8066}; // -1.5 deg offset, measured: -1.49966
         //     for(int i = 0; i < 7; i++) {
         //         gimbal_probe_samples[i] = newValues[i];
         //     }
         //     kek = kek + 1;
         // }
         // else{
-        //     float newValues[] = {-10.0760, -10.0000, -10.0760, -10.3015, -10.6699, -11.1698, -11.7861}; // 10 deg offset, measured: -10.30861
+        //     float newValues[] = {-10.0760, -10.0000, -10.0760, -10.3015, -10.6699, -11.1698, -11.7861}; // -10 deg offset, measured: -10.00034
         //     for(int i = 0; i < 7; i++) {
         //         gimbal_probe_samples[i] = newValues[i];
         //     }
         //     kek = 0;
         // }
 
-        int8_t i,j,k;
-        int8_t n = gimbal_angle_span/gimbal_step;
-
         // Create array of angle steps corresponding to each power measurement
-        float x[n + 1];
-        for(i = 0; i<=2*N; i=i+gimbal_step){
-            x[i/gimbal_step] = i - N;
+        // This method is called the Harmonic regression (or trigonometric regression)
+        // The ceoficients are calculated using Ordinary Least Square (OLS) method
+        // Polarization Loss factor is assumed equal to cos(phi)^2 which can be expressed as (1 + cos(2*phi))/2
+
+        int8_t n = gimbal_angle_span/gimbal_step;
+        float x[n+1];
+        for(int8_t i = 0; i <= 2*N; i = i+gimbal_step){
+            x[i/gimbal_step] = (i - N)*2;
         }
 
-        // Polynomial Fit using Least Squares
+        float sin_x_values[n+1];
+        float cos_x_values[n+1];
 
-        float X[5];                             //Array that will store the values of sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-        for (i=0;i<5;i++)
-        {
-            X[i]=0;
-            for (j=0;j<n+1;j++)
-                X[i]=X[i]+powf(x[j],i);          //consecutive positions of the array will store N,sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-        }
-        float B[3][4];                     //B is the Normal matrix(augmented) that will store the equations, 'a' is for value of the final coefficients
-        for (i=0;i<=2;i++)
-            for (j=0;j<=2;j++)
-                B[i][j]=X[i+j];
-
-        float Y[3];                             //Array to store the values of sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
-        for (i=0;i<3;i++)
-        {    
-            Y[i]=0;
-            for (j=0;j<n+1;j++)
-            Y[i]=Y[i]+powf(x[j],i)*gimbal_probe_samples[j];        //consecutive positions will store sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
+        for (int8_t i = 0; i < n+1; i++) {
+            sin_x_values[i] = sinf(x[i]*DEG_TO_RAD);
+            cos_x_values[i] = cosf(x[i]*DEG_TO_RAD);
         }
 
-        for (i=0;i<=2;i++)
-            B[i][3]=Y[i];                       //load the values of Y as the last column of B(Normal Matrix but augmented) 
+        float sin_mean = mean(sin_x_values, n+1);
+        float cos_mean = mean(cos_x_values, n+1);
+        float y_mean = mean(gimbal_probe_samples, n+1);
+
+        float A1 = (dot_product(gimbal_probe_samples, sin_x_values, n+1) - y_mean * sin_mean * (n+1)) /
+                    (dot_product(sin_x_values, sin_x_values, n+1) - sin_mean * sin_mean * (n+1));
+
+        float B1 = (dot_product(gimbal_probe_samples, cos_x_values, n+1) - y_mean * cos_mean * (n+1)) /
+                    (dot_product(cos_x_values, cos_x_values, n+1) - cos_mean * cos_mean * (n+1));
+
+        float D = y_mean - A1 * sin_mean - B1 * cos_mean;
+
+        float fitted_values[n+1];
+        for(int i = 0; i < n+1; i++){
+            fitted_values[i] = A1*sinf(x[i]*DEG_TO_RAD) + B1*cosf(x[i]*DEG_TO_RAD) + D;
+        }
         
-        for (i=0;i<3;i++)                       //From now Gaussian Elimination starts(can be ignored) to solve the set of linear equations (Pivotisation)
-            for (k=i+1;k<3;k++)
-                if (B[i][i]<B[k][i])
-                    for (j=0;j<=3;j++)
-                    {
-                        float temp=B[i][j];
-                        B[i][j]=B[k][j];
-                        B[k][j]=temp;
-                    }
-
-        for (i=0;i<2;i++)                       //loop to perform the gauss elimination
-            for (k=i+1;k<3;k++)
-                {
-                    float t=B[k][i]/B[i][i];
-                    for (j=0;j<=3;j++)
-                        B[k][j]=B[k][j]-t*B[i][j];    //make the elements below the pivot elements equal to zero or elimnate the variables
-                }
-
-        float a[3] = {0,0,0};
-        for (i=2;i>=0;i--)                      //back-substitution
-        {                                       //x is an array whose values correspond to the values of x,y,z..
-            a[i]=B[i][3];                       //make the variable to be calculated equal to the rhs of the last equation
-            for (j=0;j<3;j++)
-                if (j!=i)                       //then subtract all the lhs values except the coefficient of the variable whose value is being calculated
-                    a[i]=a[i]-B[i][j]*a[j];
-            a[i]=a[i]/B[i][i];                  //now finally divide the rhs by the coefficient of the variable to be calculated
-        }
-
-        // Check if we got a local maxima. Otherwise, the alignment failed
-        if(a[2] > 0 || is_zero(a[2])){
-            gcs().send_text(MAV_SEVERITY_INFO, "Gimbal alignment failed: No Maxima");
-            rotm_step.identity();
-            copter.camera_mount.set_RotM_offset(rotm_step);
-            gimbal_execute = false;
-            return;
-        }
-
-        // Evaluate resulting curve fitting at each measured angle
-        float y[n + 1];
-        for(i = 0; i<=n; i++){
-            y[i] = a[2]*x[i]*x[i] + a[1]*x[i] + a[0];
-        }
-
-        // Compute correlation coefficient
-        float sum_X = 0, sum_Y = 0, sum_XY = 0;
-        float squareSum_X = 0, squareSum_Y = 0;
-
-        for (i = 0; i <= n; i++)
-        {
-            // sum of elements of array X (sampled points)
-            sum_X = sum_X + gimbal_probe_samples[i];
-    
-            // sum of elements of array Y (output of the computed curve)
-            sum_Y = sum_Y + y[i];
-    
-            // sum of X[i] * Y[i].
-            sum_XY = sum_XY + gimbal_probe_samples[i] * y[i];
-    
-            // sum of square of array elements.
-            squareSum_X = squareSum_X + gimbal_probe_samples[i] * gimbal_probe_samples[i];
-            squareSum_Y = squareSum_Y + y[i] * y[i];
-        }
-  
-        // use formula for calculating correlation coefficient.
-        float corr = (float)(n * sum_XY - sum_X * sum_Y) 
-                    / sqrtf((n * squareSum_X - sum_X * sum_X) 
-                        * (n * squareSum_Y - sum_Y * sum_Y));
+        float corr = correlation(gimbal_probe_samples, fitted_values, n+1);
 
         // Check the correlation coefficient. Alignment failed if corr is too low
-        if(fabsf(corr) < 0.85){
+        if(fabsf(corr) < 0.70){
             gcs().send_text(MAV_SEVERITY_INFO, "Gimbal alignment failed: R = %f",corr);
             rotm_step.identity();
             copter.camera_mount.set_RotM_offset(rotm_step);
@@ -215,7 +156,7 @@ void Copter::user_ARRC_gimbal()
         }
 
         // Resultant angle offset
-        float angle_offset = -a[1]/(2*a[2]);
+        float angle_offset = 0.5*atan2f(A1,B1)*RAD_TO_DEG;
 
         gcs().send_text(MAV_SEVERITY_INFO, "Gimbal alignment correction: %f deg",angle_offset);
         gcs().send_text(MAV_SEVERITY_INFO, "Gimbal alignment success: R = %f",corr);
@@ -227,6 +168,45 @@ void Copter::user_ARRC_gimbal()
         gimbal_execute = false;
     }
 }
+
+float mean(const float* v, int n) {
+    float sum = 0.0;
+    for (int8_t i = 0; i < n; i++) {
+        sum += v[i];
+    }
+    return sum / n;
+}
+
+float dot_product(const float* v1, const float* v2, int n) {
+    double result = 0.0;
+    for (int8_t i = 0; i < n; i++) {
+        result += v1[i] * v2[i];
+    }
+    return result;
+}
+
+float standard_deviation(const float* v, float mean, int n) {
+    float sum = 0.0;
+    for (int8_t i = 0; i < n; i++) {
+        float diff = v[i] - mean;
+        sum += diff * diff;
+    }
+    return sqrtf(sum / (n - 1));
+}
+
+float correlation(const float* v1, const float* v2, int n) {
+    float mean1 = mean(v1, n);
+    float mean2 = mean(v2, n);
+    float std_dev1 = standard_deviation(v1, mean1, n);
+    float std_dev2 = standard_deviation(v2, mean2, n);
+    float sum = 0.0;
+    for (int8_t i = 0; i < n; i++) {
+        sum += (v1[i] - mean1) * (v2[i] - mean2);
+    }
+
+    return sum / ((n - 1) * std_dev1 * std_dev2);
+}
+
 #endif
 
 #ifdef USER_LB680A_LOOP
