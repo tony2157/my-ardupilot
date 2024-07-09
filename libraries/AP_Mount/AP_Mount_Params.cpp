45176c4e5184ba6088a4aa870a4fd4cf79a278ac
tony2157 <tony.segales700@gmail.com>
2024-07-08 19:18:29 -0500

    AP_Mount: Fix bad parameter enumaration

--- libraries/AP_Mount/AP_Mount_Params.cpp
+++ libraries/AP_Mount/AP_Mount_Params.cpp
@@ -175,20 +175,20 @@ const AP_Param::GroupInfo AP_Mount_Params::var_info[] = {
     // @Description: Default Target sysID for the mount to point to
     // @RebootRequired: True
     // @User: Standard
-    AP_GROUPINFO("_SYSID_DFLT", 14, AP_Mount_Params, sysid_default, 0),
+    AP_GROUPINFO("_SYSID_DFLT", 16, AP_Mount_Params, sysid_default, 0),
 
     // @Param: _DEVID
     // @DisplayName: Mount Device ID
     // @Description: Mount device ID, taking into account its type, bus and instance
     // @User: Advanced
-    AP_GROUPINFO_FLAGS("_DEVID", 15, AP_Mount_Params, dev_id, 0, AP_PARAM_FLAG_INTERNAL_USE_ONLY),
+    AP_GROUPINFO_FLAGS("_DEVID", 17, AP_Mount_Params, dev_id, 0, AP_PARAM_FLAG_INTERNAL_USE_ONLY),
 
     // @Param: _OPTIONS
     // @DisplayName: Mount options
     // @Description: Mount options bitmask
     // @Bitmask: 0:RC lock state from previous mode
     // @User: Standard
-    AP_GROUPINFO("_OPTIONS", 16, AP_Mount_Params, options, 0),
+    AP_GROUPINFO("_OPTIONS", 18, AP_Mount_Params, options, 0),
 
     AP_GROUPEND
 };
